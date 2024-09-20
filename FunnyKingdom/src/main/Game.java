908359779d3a9d1ef6ba8a13368fd2f3dd70546e
Admin <Admin@DESKTOP-NBARF9J>
2024-09-20 21:54:30 +0700

    change the second lilne

--- FunnyKingdom/src/main/Game.java
+++ FunnyKingdom/src/main/Game.java
@@ -4,7 +4,7 @@ public class Game {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hello world Quochello");
+		System.out.println("Hello world Quochello name háidfisđfdsff");
 	}
 
 }
