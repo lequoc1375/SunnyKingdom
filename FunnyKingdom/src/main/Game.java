2421088d8b1047affe212d5ce8e0a2380926c175
lequoc1375 <110074051+lequoc1375@users.noreply.github.com>
2024-09-20 21:17:31 +0700

    Update Game.java

--- FunnyKingdom/src/main/Game.java
+++ FunnyKingdom/src/main/Game.java
@@ -4,7 +4,7 @@ public class Game {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hello world 2");
+		System.out.println("Hello world Quoc");
 	}
 
 }
